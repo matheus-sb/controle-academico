4736d377cb62e2deb74345d05d936b673b419f8b
Matheus <matheus.sob@gmail.com>
2017-09-18 13:44:55 -0300

    Resolvido problema no loading

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -16,10 +16,46 @@ div
                 </span>            
 </template>
 
-<style>
+<style scoped>
   body{
     background: #666 !important;
   }
+  .custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }  
 </style>
 
 <script>
